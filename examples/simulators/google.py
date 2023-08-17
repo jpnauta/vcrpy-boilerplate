262937247b2ebdb1019207dd0eb7a5aba054e3f9
Jeremy Nauta <jeremypnauta@gmail.com>
2023-08-16 23:41:44 -0600

    Fix typo

--- examples/simulators/google.py
+++ examples/simulators/google.py
@@ -4,8 +4,8 @@ from simulations import BaseSimulator, SimulationFactory
 class GoogleSimulator(BaseSimulator):
     def simulate_home_page(self, factory: SimulationFactory):
         # When this simulation is run:
-        # - If re-playing cassettes, it will load `fixtures/cassettes/google/home_page.yaml`
-        # - If recording cassettes, it will record any requests in `fixtures/cassettes/google/home_page.yaml`
+        # - If re-playing cassettes, it will load `cassettes/google/home_page.yaml`
+        # - If recording cassettes, it will record any requests in `cassettes/google/home_page.yaml`
         factory.use_cassette("google/home_page")
 
     def simulate_search(self, factory: SimulationFactory):
